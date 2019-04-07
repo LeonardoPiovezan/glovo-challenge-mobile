3b9f3f3a85331e6860f0793fcbe9b5c3b40e97b2
Leonardo Piovezan <leonardoapiovezan@gmail.com>
2019-04-07 19:39:40 -0300

    Add constants file

---
+++ client/Maps/Maps/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Maps
+//
+//  Created by Leonardo Piovezan on 07/04/19.
+//  Copyright © 2019 Leonardo Piovezan. All rights reserved.
+//
+
+struct Constants {
+    private init() {}
+    struct Network {
+        private init() {}
+        static let baseURL = "http://localhost:3000/api"
+    }
+}
